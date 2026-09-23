410d4345c8cdf99fc368223e60a6df6c3ab93a33
Rojemmarc Gutierrez <gutierrezrojemmarc4@gmail.com>
2026-09-23 14:34:06 +0800

    Add $1 minimum fee - gutierrez.rojemmarc

--- catalog.js
+++ catalog.js
@@ -7,7 +7,7 @@ function calculateLateFee(daysLate, ratePerDay) {
     return 0;
   }
   const fee = Math.round(daysLate * ratePerDay);
-  return Math.min(fee, 20);
+  return Math.min(Math.max(fee, 1), 20);
 }
 
 module.exports = { isValidLoan, calculateLateFee };
